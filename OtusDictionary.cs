93c06d7b78c334b51160702454ab91cbce8e0a57
YuliaLegkova <136889148+YuliaLegkova@users.noreply.github.com>
2024-04-17 00:26:32 +0700

    Add files via upload

---
+++ OtusDictionary.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_12
+{
+    public class OtusDictionary
+    {
+        private const int InitialCapacity = 32;
+
+        private (int key, string value)[] items;
+        private int count;
+
+        public OtusDictionary()
+        {
+            items = new (int, string)[InitialCapacity];
+        }
+
+        public void Add(int key, string value)
+        {
+            try
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+
+                if (count == items.Length)
+                {
+                    Resize();
+                }
+
+                int index = key % items.Length;
+
+                while (items[index].value != null)
+                {
+
+                    if (items[index].key == key)
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    index = (index + 1) % items.Length;
+
+                }
+
+                items[index] = (key, value);
+                count++;
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Значение не может быть пустым");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Элемент с таким ключом уже содержится в словаре");
+            }
+
+        }
+        private void Resize()
+        {
+            var newArray = new (int key, string value)[items.Length * 2];
+
+            foreach (var (currentKey, currentValue) in items)
+            {
+                if (currentValue != null)
+                {
+                    int index = currentKey % newArray.Length;
+
+                    while (newArray[index].value != null)
+                    {
+                        index = (index + 1) % newArray.Length;
+                    }
+                    newArray[index] = (currentKey, currentValue);
+                }
+            }
+
+            items = newArray;
+        }
+        public string Get(int key)
+        {
+            try
+            {
+                int index = key % items.Length;
+
+                while (items[index].value != null)
+                {
+
+                    if (items[index].key == key)
+                    {
+                        return items[index].value;
+                    }
+
+                    index = (index + 1) % items.Length;
+                }
+
+                throw new KeyNotFoundException();
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Ключ не был найден в словаре");
+                return null;
+            }
+        }
+
+        public string this[int key]
+        {
+            get => Get(key);
+            set => Add(key, value);
+        }
+        public void PrintAll()
+        {
+            foreach (var (key, value) in items)
+            {
+                if (value != null)
+                {
+                    Console.WriteLine($"Key: {key}, Value: {value}");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace DZ_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+                var dictionary = new OtusDictionary();
+
+                dictionary.Add(1, "Один");
+                dictionary.Add(2, "Два");
+                dictionary.Add(3, "Три");
+                dictionary.Add(4, "Четыре");
+                dictionary.Add(5, "Пять");
+                dictionary.Add(6, "Шесть");
+                dictionary.Add(7, "Семь");
+                dictionary.Add(8, "Восемь");
+                dictionary.Add(9, "Девять");
+                dictionary.Add(10, "Десять");
+                dictionary.Add(11, "Одинадцать");
+                dictionary.Add(12, "Двенадцать");
+                dictionary.Add(13, "Тринадцать");
+                dictionary.Add(14, "Четырнадцать");
+                dictionary.Add(15, "Пятнадцать");
+                dictionary.Add(16, "Шестнадцать");
+                dictionary.Add(17, "Семнадцать");
+                dictionary.Add(18, "Восемнадцать");
+                dictionary.Add(19, "Девятнадцать");
+                dictionary.Add(20, "Двадцать");
+                dictionary.Add(21, null);
+                dictionary.Add(22, "22");
+                dictionary.Add(23, "23");
+                dictionary.Add(24, "24");
+                dictionary.Add(25, "25");
+                dictionary.Add(26, "26");
+                dictionary.Add(27, "27");
+                dictionary.Add(28, "28");
+                dictionary.Add(29, "29");
+                dictionary.Add(30, "Тридцать");
+                dictionary.Add(31, "Тридцать один");
+                dictionary.Add(32, "Тридцать два");
+                dictionary.Add(33, "Тридцать три");
+                
+                Console.WriteLine(dictionary.Get(3)); 
+                Console.WriteLine(dictionary[2]);   
+                dictionary[34] = "Тридцать четыре";
+                dictionary[21] = "Двадцать один";
+
+                Console.WriteLine(dictionary.Get(35));
+                Console.WriteLine(dictionary.Get(36));
+
+                dictionary.PrintAll();
+         }
+    }
+}
+
+
